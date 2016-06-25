1874bc49cc9cb3d61934a51ad01e2c504a8f69af
phearom <phearom.pov2011@gmail.com>
2016-06-25 20:32:16 +0700

    Initial commit

---
+++ app/src/main/java/com/phearom/balancing/MainActivity.java
@@ -0,0 +1,76 @@
+package com.phearom.balancing;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.phearom.balancing.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding mBinding;
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavViewLeft;
+    private NavigationView mNavViewRight;
+    private ActionBarDrawerToggle mToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setSupportActionBar(mBinding.toolbar);
+
+        mDrawerLayout = mBinding.drawer;
+        mNavViewLeft = mBinding.navLeft;
+        mNavViewRight = mBinding.navRight;
+
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mBinding.toolbar, R.string.app_name, R.string.app_name);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mDrawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                if (drawerView.getId() == mBinding.navLeft.getId()) {
+                    mDrawerLayout.closeDrawer(GravityCompat.END);
+                } else {
+                    mDrawerLayout.closeDrawer(GravityCompat.START);
+                }
+                mToggle.syncState();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+            }
+        });
+        mToggle.syncState();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_option:
+                mDrawerLayout.openDrawer(GravityCompat.END);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
